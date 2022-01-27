69980dcf4dfef8aaa8b695a5dd278d6f4f4332ae
jihy1218 <rlawlgud1108@gmail.com>
2022-01-27 10:28:44 +0900

    27일 오전 커밋

--- src/main/java/movie/controller/MovieController.java
+++ src/main/java/movie/controller/MovieController.java
@@ -1,5 +1,7 @@
 package movie.controller;
 
+import movie.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +18,8 @@ public class MovieController {
 
     @GetMapping("/ticketingseat")
     public  String ticketingseat(){return "movie/ticketingseat";}
+
+    @Autowired
+    private MovieService movieService;
 
 }
