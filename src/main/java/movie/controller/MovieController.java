bcb2d43d43ff727b508ce08949a52693ed9af820
jihy1218 <rlawlgud1108@gmail.com>
2022-02-10 17:25:40 +0900

    10일  날짜 뽑아오기까지 성공

--- src/main/java/movie/controller/MovieController.java
+++ src/main/java/movie/controller/MovieController.java
@@ -1,14 +1,17 @@
 package movie.controller;
 
 import movie.domain.Dto.MovieinfoDto;
+import movie.service.DateService;
 import movie.service.MovieService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,13 @@ public class MovieController {
     public String ticketing(Model model){
         List<MovieinfoDto> movielist = movieService.getmovieinfo();
         model.addAttribute("movielist",movielist);
+        String dates=null;
+        for(MovieinfoDto temp : movielist){
+            dates =dateService.datelist(temp.getMvno());
+
+        }
+        System.out.println(dates+"나오냐요오");
+        model.addAttribute("datelist",dates);
         return "movie/ticketingdate";
     }
 
@@ -30,23 +40,32 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @Autowired
+    private DateService dateService;
     // 지형 여기까지 여기에 잠들다.....
-   /* @GetMapping("/movieview")
-    public String movieview(Model model){ // 영화번호에 해당하는 엔티티 뽑아와야뎀
-        JSONObject jsonObject = movieService.getmovieinfoselect();
-        List<MovieinfoDto> movieinfoDtos = new ArrayList<>();
+
+   @GetMapping("/movieview/{mvid}")
+    public String movieview(@PathVariable("mvid")String mvid, Model model){ // 영화번호에 해당하는 엔티티 뽑아와야뎀
+        JSONObject jsonObject = movieService.getmovieinfoselect(mvid);
         MovieinfoDto movieinfoDto = MovieinfoDto.builder()
+                .mvno(movieService.getMvno(mvid))
+                .mvid(mvid)
                 .movieNm((String)jsonObject.get("movieNm"))
                 .openDt((String)jsonObject.get("openDt"))
-                .nations((String)jsonObject.get("nationNm"))
-                .genres((String)jsonObject.get("genreNm"))
-                .directors((String)jsonObject.get("peopleNm"))
+                .showTm(Integer.valueOf((String) jsonObject.get("showTm")))
+                .nations((String)jsonObject.get("nations"))
+                .genres((String)jsonObject.get("genres"))
+                .directors((String)jsonObject.get("directors"))
                 .actors((String)jsonObject.get("actors"))
+                .companyNm((String)jsonObject.get("companyNm"))
                 .watchGradeNm((String)jsonObject.get("watchGradeNm"))
+                .poster((String)jsonObject.get("poster"))
+                .movieimg((List<String>)jsonObject.get("movieimg"))
+                .movievideo((List<String>)jsonObject.get("movievideo"))
                 .build();
-        movieinfoDtos.add(movieinfoDto);
-        model.addAttribute("movie",movieinfoDtos);
+       System.out.println(movieinfoDto.toString()+"영화상세정보");
+        model.addAttribute("movieview",movieinfoDto);
         return "movie/movieview";
-    }*/
+    }
 
 }
--- src/main/java/movie/domain/Entity/Date/DateRepository.java
+++ src/main/java/movie/domain/Entity/Date/DateRepository.java
@@ -1,6 +1,12 @@
 package movie.domain.Entity.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DateRepository extends JpaRepository<DateEntity,Integer> {
+    @Query(nativeQuery = true,value = "select ddate from date where mvno=:mvno")
+    List<String> findDateByMvno(@Param("mvno")int mvno);
 }
